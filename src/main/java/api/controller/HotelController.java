8acc26ecfb65d4062cef5dd50971ea06764fd065
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-06-07 21:47:12 +0200

    Update HotelController.java

--- src/main/java/api/controller/HotelController.java
+++ src/main/java/api/controller/HotelController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,6 +44,18 @@ public class HotelController {
 		
 		hotel_id=HotelServiceImpl.HotelXID(id);
 		return hotel_id;
+	}
+	
+	// Devulve los hoteles pertenecientes solo a la empresa autenticada
+	@PreAuthorize("hasAnyAuthority('ROLE_EMPRESA')")
+	@GetMapping("/hoteles/empresa")
+	public List<Hotel> getHotelesOfEmpresa() {
+		
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        
+        System.out.println(authentication);
+
+		return null;
 	}
 	
 	@PutMapping("/hoteles/{id}")
