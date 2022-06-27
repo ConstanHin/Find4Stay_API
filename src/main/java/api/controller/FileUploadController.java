80d04864d35dfcd3200c70e9d1ed296eb7fa530e
Gerard Viñuela <gerardviru@gmail.com>
2022-06-27 09:30:54 +0200

    Intento recogida de foto

--- src/main/java/api/controller/FileUploadController.java
+++ src/main/java/api/controller/FileUploadController.java
@@ -1,9 +1,15 @@
 package api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,6 +25,14 @@ public class FileUploadController {
 	@Autowired
 	public FileUploadController(StorageService storageService) {
 		this.storageService = storageService;
+	}
+	
+	@GetMapping("/file/{filename}")
+	@ResponseBody
+	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+		
+			Resource file = storageService.loadAsResource(filename);
+			return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\"" + file.getFilename() + "\"").body(file);
 	}
 	
 	@PostMapping("/file/add")
